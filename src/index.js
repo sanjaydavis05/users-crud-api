415ab4c671565a3a304f806b2b681cf76dccb6c3
sanjaydavis <psanjuknl@gmail.com>
2026-06-14 15:47:07 +0530

    Strip trailing newlines from URLs to fix Postman paste issues

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const { errorHandler, notFound } = require('./middleware/error');
 
 const app = express();
 
+app.use((req, _res, next) => { req.url = req.url.replace(/(%0A|%0D|%0a|%0d|[\r\n])+$/g, ''); next(); });
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
